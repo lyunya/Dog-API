a029356896e01927b246b5811da42211d6739dbd
Leon Marbukh <leonmarbukh@gmail.com>
2020-03-17 09:03:12 -0700

    got random dog breed pics to show

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-function dogPicNum(){ $("#submit").click(function(){
+function dogPicNum(){ 
+    $("#submit").click(function(event){
+    event.preventDefault();
     var str = $("#quantity").val();
-    return str;
-)};
+    //console.log(str);
+    return getDogImages(str);
+});
+};
+
+function getDogImages(input) {
+    fetch(`https://dog.ceo/api/breeds/image/random/${input}` )
+      .then(response => response.json())
+      .then(responseJson => 
+        displayResults(responseJson.message)
+        )
+      .catch(error => alert('Something went wrong. Try again later.'));
+  }
+  
+  function displayResults(responseJson) {
+    $('.results').removeClass('hidden');
+    $('.image-container').empty();
+    console.log(responseJson);
+    const result = responseJson.map(item => {
+        //$('.results').replaceWith(
+            return `<img src="${item}" class="results-img">`
+    //)
+})
+$('.image-container').append(result);
+  }
+
+dogPicNum();
